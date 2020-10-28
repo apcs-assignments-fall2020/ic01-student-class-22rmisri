14bf363c8dbf08e03f50373a0a98dce47c75e18d
22rmisri <22rmisri@athenian.org>
2020-10-28 19:26:18 +0000

    wrote code for student class using inheritance

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,56 +1,37 @@
-public class Student {
-    private String firstname;
-    private String lastname;
-    private int classyear;
+public class Student extends Person {
+    private int classYear;
 
-    public Student(String f, String l, int c) {
-        this.firstname = f;
-        this.lastname = l;
-        this.classyear = c;
+    public Student(String a, String b, int c) {
+        super(a, b);
+        // this.firstName = a;
+        // this.lastName = b;
+        this.classYear = c;
     }
+
     public Student() {
-        this.firstname = "John";
-        this.lastname = "Smith";
-        this.classyear = 2022;
+        super("Jerry", "Seinfield");
+        // this.firstName = "Jerry";
+        // this.lastName = "Seinfield";
+        this.classYear = 2024;
+
     }
 
-    public void doSomething() {
-        System.out.println("The student is doing homeowork");
-    }
     public String toString() {
-        String str = firstname + " " + lastname + "," + classyear;
+        String str = this.getFirstName() + " " + this.getLastName() + ", " + this.classYear;
         return str;
     }
 
-    public String getFirstName() {
-        return this.firstname;
-    }
-
-    public String getLastName() {
-        return this.lastname;
-    }
     public int getclassYear() {
-        return this.classyear;
+        return this.classYear;
     }
-
-    public void setFirstName(String firstname) {
-        if (firstname.trim().length() != 0) {
-            this.firstname = firstname;
+    public void setclassYear(int classYear) {
+        if (classYear > 2020 && classYear <= 2025) {
+            this.classYear = classYear;
         }
-    }
-
-    public void setLastName(String lastname) {
-        if (lastname.trim().length() != 0) {
-            this.lastname = lastname;
-        }
-    }
-
-    public void setclassYear(int classyear) {
-        if (classyear > 2020 && classyear <=2033) {
-            this.classyear = classyear;
-        }
+        
     }
 
 
-    
+
+
 }
--- src/main/java/StudentClient.java
+++ src/main/java/StudentClient.java
@@ -1,5 +1,9 @@
 public class StudentClient {
     public static void main(String[] args) {
-        
+        Student s1 = new Student("Bob", "Dylan", 2021);
+        System.out.println(s1);
+
+        Student s2 = new Student();
+        System.out.println(s2);
     }
 }
